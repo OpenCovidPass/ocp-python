869b993d9813520bc3bd9fc7f6e3fa7e1b380cd8
Cyclic3 <cyclic3.git@gmail.com>
2021-04-19 16:47:54 +0100

    minify

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # OpenCovidPass
 
-The main.py script can perform all the tasks, the client.py only contains the code required for qr generation
+The main.py script can perform all the tasks, the client.py only contains the code required for qr generation. client-min.py is a low quality, but simple implementation of the client
--- client-min.py
+++ client-min.py
@@ -1,8 +1,4 @@
-import wand.image
-import qrcode
-import sys
-import hashlib
-import base64
+import wand.image,qrcode,sys,hashlib,base64
 
 def generate_qr(data: bytes):
   qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=20, border=4)
@@ -11,27 +7,19 @@ def generate_qr(data: bytes):
 
 def compress_img(filename: str, output: str):
   img = wand.image.Image(filename=filename)
-  sq = min(img.size[0], img.size[1])
-  img.crop(width=sq, height=sq, gravity="center")
   img.resize(200, 200)
   img.quantize(8, "gray")
   img.save(filename=output)
 
 def chunk_data(data: bytes):
   data = base64.b32encode(data).decode().rstrip('=')
-  number = 6
-  chunk_size = len(data) // number
-  if (number > 10):
-    raise Exception("Too much data to store in qr codes!")
-  return [f'{i}{number - 1}' + data[i * chunk_size: (i+1) * chunk_size] for i in range(number)]
+  chunk_size = len(data) // 6
+  return [f'{i}{number - 1}' + data[i * chunk_size: (i+1) * chunk_size] for i in range(6)]
 
 def encode(compressed, sig, out):
-  if len(sig) != 128:
-    raise Exception("Invalid signature size")
   chunks = chunk_data(base64.b16decode(sig.upper()) + compressed)
   for i in range(len(chunks)):
     generate_qr(chunks[i]).save(f"{out}/{i}.png")
-
 
 # MAIN CODE
 input_pici = sys.argv[1]
