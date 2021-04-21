51f92462091f8ab51b6f45785423c27ae0539917
Cyclic3 <cyclic3.git@gmail.com>
2021-04-21 13:12:44 +0100

    Update client-min.py

--- client-min.py
+++ client-min.py
@@ -1,30 +1,14 @@
 import wand.image,qrcode,sys,hashlib,base64
-
-def generate_qr(data: bytes):
-  qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=20, border=4)
-  qr.add_data(data)
-  return qr.make_image(fill_color="black", back_color="white")
-
-def compress_img(filename: str, output: str):
-  img = wand.image.Image(filename=filename)
-  img.resize(200, 200)
-  img.quantize(8, "gray")
-  img.save(filename=output)
-
-def chunk_data(data: bytes):
-  data = base64.b32encode(data).decode().rstrip('=')
-  chunk_size = len(data) // 6
-  return [f'{i}{number - 1}' + data[i * chunk_size: (i+1) * chunk_size] for i in range(6)]
-
-def encode(compressed, sig, out):
-  chunks = chunk_data(base64.b16decode(sig.upper()) + compressed)
-  for i in range(len(chunks)):
-    generate_qr(chunks[i]).save(f"{out}/{i}.png")
-
-# MAIN CODE
-compress_img(sys.argv[1],  sys.argv[2])
+img = wand.image.Image(filename=sys.argv[1])
+img.resize(200, 200)
+img.quantize(8, "gray")
+img.save(filename=sys.argv[2])
 with open(sys.argv[2], "rb") as file:
   data = file.read()
   print(hashlib.sha256(data).hexdigest())
-  sig = input("Signature: ")
-  encode(data, sig, sys.argv[3])
+  data = base64.b32encode(base64.b16decode(input("Signature: ").upper()) + data).decode().rstrip('=')
+  number = 6; chunks = [f'{i}{number}' + data[i * (len(data) // number): (i+1) * (len(data) // number)] for i in range(number)]
+  for i in range(len(chunks)):
+    qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=20, border=4)
+    qr.add_data(chunks[i])
+    qr.make_image(fill_color="black", back_color="white").save(f"{sys.argv[3]}/{i}.png")
